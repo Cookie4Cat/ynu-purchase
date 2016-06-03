55c55c093f82d819ecab153518f5afea8b1d687c
Cookie4Cat <guozy@foxmail.com>
2016-06-03 10:10:11 +0800

    增加tokenUtil伪实现类

---
+++ src/edu/ynu/util/TokenUtil.java
@@ -0,0 +1,27 @@
+package edu.ynu.util;
+
+public class TokenUtil {
+
+    public String getToken(int userId){
+        if(userId==1){
+            return "1234";
+        }
+        else if(userId==2) {
+            return "abcd";
+        }
+        return "null";
+    }
+    public boolean validate(String token){
+        return token.equals("abcd")||token.equals("1234");
+    }
+    public int getUserFormToken(String token){
+        switch (token){
+            case "1234":
+                return 1;
+            case "abcd":
+                return 2;
+            default:
+                return 0;
+        }
+    }
+}
