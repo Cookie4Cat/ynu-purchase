e30f2a49de6f2e766a367517465d88c9b2cc00d3
Cookie4Cat <guozy@foxmail.com>
2016-06-03 12:03:31 +0800

    ...

--- src/edu/ynu/util/TokenUtil.java
+++ src/edu/ynu/util/TokenUtil.java
@@ -1,9 +1,11 @@
 package edu.ynu.util;
 
 import edu.ynu.dao.impl.TokenDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TokenUtil {
 
+	@Autowired
 	private TokenDaoImpl tokenDao;
 	public String getToken(String userId) {
 		return tokenDao.getToken(userId);
