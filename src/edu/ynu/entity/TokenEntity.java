b99d1d3fe7d95ab89161486ee03f40aff0f4c0a4
Cookie4Cat <guozy@foxmail.com>
2016-06-03 11:39:50 +0800

    增加token实体

---
+++ src/edu/ynu/entity/TokenEntity.java
@@ -0,0 +1,77 @@
+package edu.ynu.entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "token", schema = "purchase", catalog = "")
+public class TokenEntity {
+    private int tokenId;
+    private int userId;
+    private String token;
+    private Timestamp dateline;
+
+    @Id
+    @Column(name = "token_id", nullable = false)
+    public int getTokenId() {
+        return tokenId;
+    }
+
+    public void setTokenId(int tokenId) {
+        this.tokenId = tokenId;
+    }
+
+    @Basic
+    @Column(name = "user_id", nullable = false)
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "token", nullable = false, length = 100)
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Basic
+    @Column(name = "dateline", nullable = false)
+    public Timestamp getDateline() {
+        return dateline;
+    }
+
+    public void setDateline(Timestamp dateline) {
+        this.dateline = dateline;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TokenEntity that = (TokenEntity) o;
+
+        if (tokenId != that.tokenId) return false;
+        if (userId != that.userId) return false;
+        if (token != null ? !token.equals(that.token) : that.token != null) return false;
+        if (dateline != null ? !dateline.equals(that.dateline) : that.dateline != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = tokenId;
+        result = 31 * result + userId;
+        result = 31 * result + (token != null ? token.hashCode() : 0);
+        result = 31 * result + (dateline != null ? dateline.hashCode() : 0);
+        return result;
+    }
+}
