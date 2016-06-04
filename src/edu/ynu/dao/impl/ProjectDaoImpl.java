095b44cfa4b265542137f4f9ee3f4f826a43eb92
LiquckTeng <2606876435@qq.com>
2016-06-04 19:12:56 +0800

    增加ProjectDao实现类

--- src/edu/ynu/dao/impl/ProjectDaoImpl.java
+++ src/edu/ynu/dao/impl/ProjectDaoImpl.java
@@ -2,12 +2,31 @@ package edu.ynu.dao.impl;
 
 import edu.ynu.dao.ProjectDao;
 import edu.ynu.entity.ProjectEntity;
+import edu.ynu.entity.TokenEntity;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class ProjectDaoImpl implements ProjectDao {
+    private  SessionFactory sessionFactory;
+    @Autowired
+    public ProjectDaoImpl(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+    private Session currentSession(){
+        return this.sessionFactory.getCurrentSession();
+    }
     @Override
     public List<ProjectEntity> getProjectsByUID(String userId) {
-        return null;
+        String hql = "from ProjectEntity projects where projects.userId=:userIdContent";
+        Query query = this.currentSession().createQuery(hql);
+        query.setString("userIdContent",userId);
+        List projectList = query.list();
+        return projectList;
     }
 }
