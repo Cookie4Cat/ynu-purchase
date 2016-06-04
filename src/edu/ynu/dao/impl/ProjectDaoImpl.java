aa924e433ec1ee625dc7798a30c292514b0ac3d4
Cookie4Cat <guozy@foxmail.com>
2016-06-04 18:08:23 +0800

    修改tokendao，基本实现了所需功能

---
+++ src/edu/ynu/dao/impl/ProjectDaoImpl.java
@@ -0,0 +1,13 @@
+package edu.ynu.dao.impl;
+
+import edu.ynu.dao.ProjectDao;
+import edu.ynu.entity.ProjectEntity;
+
+import java.util.List;
+
+public class ProjectDaoImpl implements ProjectDao {
+    @Override
+    public List<ProjectEntity> getProjectsByUID(String userId) {
+        return null;
+    }
+}
