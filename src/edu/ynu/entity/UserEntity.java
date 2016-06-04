cc57a52c2562e22735bca1fc3b893441f5ba059c
Cookie4Cat <guozy@foxmail.com>
2016-06-04 10:50:30 +0800

    =-=

--- src/edu/ynu/entity/UserEntity.java
+++ src/edu/ynu/entity/UserEntity.java
@@ -3,7 +3,7 @@ package edu.ynu.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user", schema = "purchase", catalog = "")
+@Table(name = "user", schema = "purchase")
 public class UserEntity {
 	private String userId;
 	private String username;
