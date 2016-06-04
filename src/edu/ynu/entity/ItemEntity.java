7d9e1bf5f868012af36ecab48f0c3e96d30ff49d
LiquckTeng <2606876435@qq.com>
2016-06-04 17:55:13 +0800

    =-=

---
+++ src/edu/ynu/entity/ItemEntity.java
@@ -0,0 +1,85 @@
+package edu.ynu.entity;
+
+import org.hibernate.annotations.*;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by Administrator on 2016/6/4.
+ */
+@Entity
+@Table(name = "item", schema = "purchase")
+public class ItemEntity {
+    private int id;
+    private String itemName;
+    private String count;
+    private String price;
+    private String deliverySite;
+    private ProjectEntity project;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", unique = true, nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "count", nullable = false, length = 45)
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    @Basic
+    @Column(name = "delivery_site", nullable = false, length = 200)
+    public String getDeliverySite() {
+        return deliverySite;
+    }
+
+    public void setDeliverySite(String deliverySite) {
+        this.deliverySite = deliverySite;
+    }
+
+    @Basic
+    @Column(name = "item_name", nullable = false, length = 45)
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    @Basic
+    @Column(name = "price", nullable = false, length = 45)
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    //双向绑定
+    //配置item实体和project实体多对一的关系
+    @ManyToOne(fetch = FetchType.EAGER)
+    @Cascade(value = {org.hibernate.annotations.CascadeType.SAVE_UPDATE})
+    @JoinColumn(name = "project_id")
+    public ProjectEntity getProject() {
+        return project;
+    }
+
+    public void setProject(ProjectEntity project) {
+        this.project = project;
+    }
+}
---
+++ src/edu/ynu/entity/ProjectEntity.java
@@ -0,0 +1,196 @@
+package edu.ynu.entity;
+
+import org.hibernate.annotations.*;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Administrator on 2016/6/4.
+ */
+@Entity
+@Table(name = "purchase_project", schema = "purchase")
+public class ProjectEntity {
+    private int id;
+    private String projectId;
+    private String userId;
+    private String proposerName;
+    private String proposerTel;
+    private String agentName;
+    private String agentTel;
+    private String projectName;
+    private String purchaseType;
+    private String sum;
+    private String fundSource;
+    private String applyReason;
+    private String submitTime;
+    private String comment;
+    private String status;
+    private Set<ItemEntity> items = new HashSet<ItemEntity>();
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", unique = true, nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "user_id", nullable = false, length = 45)
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "agent_name", nullable = false, length = 45)
+    public String getAgentName() {
+        return agentName;
+    }
+
+    public void setAgentName(String agentName) {
+        this.agentName = agentName;
+    }
+
+    @Basic
+    @Column(name = "agent_tel", nullable = false, length = 45)
+    public String getAgentTel() {
+        return agentTel;
+    }
+
+    public void setAgentTel(String agentTel) {
+        this.agentTel = agentTel;
+    }
+
+    @Basic
+    @Column(name = "apply_reason", nullable = false, length = 500)
+    public String getApplyReason() {
+        return applyReason;
+    }
+
+    public void setApplyReason(String applyReason) {
+        this.applyReason = applyReason;
+    }
+
+    @Basic
+    @Column(name = "comment", nullable = false, length = 500)
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Basic
+    @Column(name = "fund_source", nullable = false, length = 200)
+    public String getFundSource() {
+        return fundSource;
+    }
+
+    public void setFundSource(String fundSource) {
+        this.fundSource = fundSource;
+    }
+
+    @Basic
+    @Column(name = "project_id", nullable = false, length = 100)
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    @Basic
+    @Column(name = "proposer_name", nullable = false, length = 45)
+    public String getProposerName() {
+        return proposerName;
+    }
+
+    public void setProposerName(String proposerName) {
+        this.proposerName = proposerName;
+    }
+
+    @Basic
+    @Column(name = "project_name", nullable = false, length = 45)
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    @Basic
+    @Column(name = "proposer_tel", nullable = false, length = 45)
+    public String getProposerTel() {
+        return proposerTel;
+    }
+
+    public void setProposerTel(String proposerTel) {
+        this.proposerTel = proposerTel;
+    }
+
+    @Basic
+    @Column(name = "purchase_type", nullable = false, length = 45)
+    public String getPurchaseType() {
+        return purchaseType;
+    }
+
+    public void setPurchaseType(String purchaseType) {
+        this.purchaseType = purchaseType;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = false, length = 45)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "submit_time", nullable = false, length = 100)
+    public String getSubmitTime() {
+        return submitTime;
+    }
+
+    public void setSubmitTime(String submitTime) {
+        this.submitTime = submitTime;
+    }
+
+    @Basic
+    @Column(name = "sum", nullable = false, length = 45)
+    public String getSum() {
+        return sum;
+    }
+
+    public void setSum(String sum) {
+        this.sum = sum;
+    }
+
+    //双向绑定
+    //配置project实体与item实体一对多的关系
+    @OneToMany(mappedBy = "project", fetch = FetchType.EAGER)
+    @Cascade(value = {org.hibernate.annotations.CascadeType.DELETE})
+    public Set<ItemEntity> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<ItemEntity> items) {
+        this.items = items;
+    }
+}
