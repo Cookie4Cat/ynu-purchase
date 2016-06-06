3421fbdd4c6e15c7773d63e0c6d466fb188faf74
Cookie4Cat <guozy@foxmail.com>
2016-06-06 18:20:15 +0800

    init

--- src/edu/ynu/dao/impl/AssetsDaoImpl.java
+++ src/edu/ynu/dao/impl/AssetsDaoImpl.java
@@ -2,40 +2,39 @@ package edu.ynu.dao.impl;
 
 import edu.ynu.dao.AssetsDao;
 import edu.ynu.entity.AssetsEntity;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
-public class AssetsDaoImpl implements AssetsDao {
-    private SessionFactory sessionFactory;
+import java.util.List;
+
+public class AssetsDaoImpl extends BaseDao<AssetsEntity> implements AssetsDao {
 
     @Autowired
-    public AssetsDaoImpl(SessionFactory sessionFactory){
-        this.sessionFactory = sessionFactory;
-    }
-    private Session currentSession(){
-        return this.sessionFactory.getCurrentSession();
+    public AssetsDaoImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
     }
 
     @Override
     public void addAssets(AssetsEntity assets) {
-        this.currentSession().save(assets);
+
     }
 
     @Override
     public void deleteAssets(int id) {
-        AssetsEntity assets = (AssetsEntity)this.currentSession().get(AssetsEntity.class,id);
-        this.currentSession().delete(assets);
+
     }
 
     @Override
     public void updateAssets(AssetsEntity assets) {
-        this.currentSession().update(assets);
+
     }
 
     @Override
     public AssetsEntity getAssets(int id) {
-        return (AssetsEntity) this.currentSession().get(AssetsEntity.class,id);
+        return null;
+    }
+    @Override
+    public List<AssetsEntity> findAll(){
+        return super.findAll();
     }
 }
