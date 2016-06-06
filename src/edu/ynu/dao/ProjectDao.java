bb44816c7955fedb00384b024c2def39cc338603
Cookie4Cat <guozy@foxmail.com>
2016-06-06 11:00:33 +0800

    增加projectDao接口方法

--- src/edu/ynu/dao/ProjectDao.java
+++ src/edu/ynu/dao/ProjectDao.java
@@ -5,5 +5,25 @@ import edu.ynu.entity.ProjectEntity;
 import java.util.List;
 
 public interface ProjectDao {
+    /***
+     * 根据userId返回project实体列表
+     * @param userId 用户id
+     * @return project实体
+     */
     public List<ProjectEntity> getProjectsByUID(String userId);
+
+    /***
+     * 根据id返回project实体对象
+     * @param id project的id
+     * @return project实体对象
+     */
+    public ProjectEntity getProjectById(int id);
+
+    /***
+     * 根据project示例对象，返回project对象列表
+     * @param example 实例对象
+     * @return project列表
+     */
+    public List<ProjectEntity> getProjectsByExample(ProjectEntity example);
+
 }
