2e350562eb5c3805cb065724caf923b3971bf52c
Cookie4Cat <guozy@foxmail.com>
2016-06-08 12:02:24 +0800

    增加接口方法

--- src/edu/ynu/dao/ProjectDao.java
+++ src/edu/ynu/dao/ProjectDao.java
@@ -10,20 +10,43 @@ public interface ProjectDao {
      * @param userId 用户id
      * @return project实体
      */
-    public List<ProjectEntity> getProjectsByUID(String userId);
+    List<ProjectEntity> getProjectsByUID(String userId);
 
     /***
      * 根据id返回project实体对象
      * @param id project的id
      * @return project实体对象
      */
-    public ProjectEntity getProjectById(int id);
+    ProjectEntity getProjectById(int id);
 
     /***
      * 根据project示例对象，返回project对象列表
      * @param example 实例对象
      * @return project列表
      */
-    public List<ProjectEntity> getProjectsByExample(ProjectEntity example);
+    List<ProjectEntity> getProjectsByExample(ProjectEntity example);
+
+    /***
+     * 保存项目申请草稿
+     * @param userId 用户ID
+     * @param projectEntity 项目实体
+     * @return 1：成功，0：失败
+     */
+    Integer savePurchaseApplyDraft(String userId,ProjectEntity projectEntity);
+
+    /***
+     * 提交项目申请表
+     * @param userId 用户ID
+     * @param projectEntity 项目实体
+     * @return 1：成功，0：失败
+     */
+    Integer savePurchaseApply(String userId,ProjectEntity projectEntity);
+
+    /***
+     * 根据用户ID返回项目申请书草稿
+     * @param userId 用户ID
+     * @return 项目草稿List
+     */
+    List<ProjectEntity> findProjectsDraftByUID(String userId);
 
 }
