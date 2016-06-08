968e3e7a45a151853a33be36250749aa15c36681
Cookie4Cat <guozy42@foxmail.com>
2016-06-08 18:30:16 +0800

    增加新方法的测试

--- src/edu/ynu/test/userServiceTest.java
+++ src/edu/ynu/test/userServiceTest.java
@@ -1,12 +1,15 @@
 package edu.ynu.test;
 
+import edu.ynu.message.PurchaseApplySubmit;
 import edu.ynu.message.PurchaseHistoryRecord;
+import edu.ynu.message.PurchaseItem;
 import edu.ynu.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class userServiceTest {
@@ -24,6 +27,47 @@ public class userServiceTest {
 		List<PurchaseHistoryRecord> historyList = userService.findSubmitHistoryById("1");
 		for (PurchaseHistoryRecord record : historyList) {
 			System.out.println(record.toString());
+		}
+	}
+	@Test
+	public void testStoreDraft(){
+		PurchaseApplySubmit pas = new PurchaseApplySubmit();
+		pas.setProjectName("测试项目名称");
+		pas.setComeFrom("测试资金来源");
+		pas.setPurchaseTyp("测试资产类型");
+		pas.setTotalMoney_pre(25000d);
+		List<PurchaseItem> itemList = new ArrayList<>();
+		PurchaseItem item = new PurchaseItem();
+		item.setName("测试设备名称");
+		item.setType("测试设备类型");
+		itemList.add(item);
+		PurchaseItem item1 = new PurchaseItem();
+		item1.setName("测试设备名称1");
+		itemList.add(item1);
+		pas.setTable(itemList);
+		userService.storePurchaseApplyDraft("laohuang",pas);
+	}
+	@Test
+	public void testStoreApply(){
+		PurchaseApplySubmit pas = new PurchaseApplySubmit();
+		pas.setProjectName("测试项目名称");
+		pas.setPurchaseTyp("测试资产类型");
+		List<PurchaseItem> itemList = new ArrayList<>();
+		PurchaseItem item = new PurchaseItem();
+		item.setName("测试设备名称");
+		item.setType("测试设备类型");
+		itemList.add(item);
+		pas.setTable(itemList);
+		userService.submitPurchaseApply("laohuang",pas);
+	}
+	@Test
+	public void testFindAllDraft(){
+		List<PurchaseApplySubmit> applyList= userService.findStoredPurchaseApplyDraft("laohuang");
+		for(PurchaseApplySubmit pas:applyList){
+			System.out.println(pas.getProjectName());
+			for(PurchaseItem item : pas.getTable()){
+				System.out.println(item.getName());
+			}
 		}
 	}
 }
