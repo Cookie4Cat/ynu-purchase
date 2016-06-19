d65a3cbb335e810492f7ebf672cf505592ed9c61
Cookie4Cat <guozy42@foxmail.com>
2016-06-19 16:13:32 +0800

    init

---
+++ src/edu/ynu/service/impl/AdminServiceImpl.java
@@ -0,0 +1,55 @@
+package edu.ynu.service.impl;
+
+import edu.ynu.dao.ProjectDao;
+import edu.ynu.entity.ProjectEntity;
+import edu.ynu.message.PurchaseApplySubmit;
+import edu.ynu.service.AdminService;
+import edu.ynu.util.MapUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    private ProjectDao projectDao;
+    @Override
+    public Integer findAllProjectsCount() {
+        String[] statusList= {"待立项","待初审"};
+        return projectDao.countProjectListByStatus(statusList);
+    }
+
+    @Override
+    public List<PurchaseApplySubmit> findAllProjects(Integer countPerPage, Integer currentPage) {
+        String[] statusList= {"待立项","待初审"};
+        List<ProjectEntity> entityList = projectDao.findProjectListByStatus(statusList,countPerPage,currentPage);
+        return MapUtil.transfromToMessageList(entityList);
+    }
+
+    @Override
+    public List<PurchaseApplySubmit> findAllByCondition(String projectId, String type, String status) {
+        List<ProjectEntity> entityList = projectDao.findProjectsByCondition(projectId,type,status);
+        return MapUtil.transfromToMessageList(entityList);
+    }
+
+    @Override
+    public PurchaseApplySubmit findOneProject(String projectId) {
+        ProjectEntity entity = projectDao.findProjectByPId(projectId);
+        PurchaseApplySubmit message = new PurchaseApplySubmit();
+        MapUtil.mapEntity(entity,message);
+        return message;
+    }
+
+    @Override
+    public Integer addProjectSuggestion(String projectId, String suggestion) {
+        projectDao.addSuggestion(projectId,suggestion);
+        return 1;//success
+    }
+
+    @Override
+    public Integer setProjectUp(String projectId) {
+        projectDao.setProjectUp(projectId);
+        return 1;//success
+    }
+}
