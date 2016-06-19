bfdbef6dbac46e393e6b54ca042ae4936f086cbb
Cookie4Cat <guozy42@foxmail.com>
2016-06-19 16:13:40 +0800

    init

---
+++ src/edu/ynu/service/AdminService.java
@@ -0,0 +1,15 @@
+package edu.ynu.service;
+
+import edu.ynu.message.PurchaseApplySubmit;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
+public interface AdminService {
+    Integer findAllProjectsCount();
+    List<PurchaseApplySubmit> findAllProjects(Integer countPerPage,Integer currentPage);
+    List<PurchaseApplySubmit> findAllByCondition(String projectId,String type,String status);
+    PurchaseApplySubmit findOneProject(String projectId);
+    Integer addProjectSuggestion(String projectId,String suggestion);
+    Integer setProjectUp(String projectId);
+}
