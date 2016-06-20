b60bd1ab7fd5ca7ebfd6bfcf978f57ffc900eb8c
Cookie4Cat <guozy42@foxmail.com>
2016-06-20 15:19:01 +0800

    使用新的转换工具类

--- src/edu/ynu/service/impl/AdminServiceImpl.java
+++ src/edu/ynu/service/impl/AdminServiceImpl.java
@@ -4,7 +4,7 @@ import edu.ynu.dao.ProjectDao;
 import edu.ynu.entity.ProjectEntity;
 import edu.ynu.message.PurchaseApplySubmit;
 import edu.ynu.service.AdminService;
-import edu.ynu.util.MapUtil;
+import edu.ynu.util.TransformUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,20 +24,20 @@ public class AdminServiceImpl implements AdminService {
     public List<PurchaseApplySubmit> findAllProjects(Integer countPerPage, Integer currentPage) {
         String[] statusList= {"待立项","待初审"};
         List<ProjectEntity> entityList = projectDao.findProjectListByStatus(statusList,countPerPage,currentPage);
-        return MapUtil.transfromToMessageList(entityList);
+        return TransformUtil.transformToMessageList(entityList);
     }
 
     @Override
     public List<PurchaseApplySubmit> findAllByCondition(String projectId, String type, String status) {
         List<ProjectEntity> entityList = projectDao.findProjectsByCondition(projectId,type,status);
-        return MapUtil.transfromToMessageList(entityList);
+        return TransformUtil.transformToMessageList(entityList);
     }
 
     @Override
     public PurchaseApplySubmit findOneProject(String projectId) {
         ProjectEntity entity = projectDao.findProjectByPId(projectId);
         PurchaseApplySubmit message = new PurchaseApplySubmit();
-        MapUtil.mapEntity(entity,message);
+        TransformUtil.mapEntity(entity,message);
         return message;
     }
 
