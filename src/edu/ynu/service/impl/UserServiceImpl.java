505ea2a1f90ebdf61f234bff025c88317ae6ded8
Cookie4Cat <guozy42@foxmail.com>
2016-06-24 12:13:00 +0800

    实现项目编号的自动生成

--- src/edu/ynu/service/impl/UserServiceImpl.java
+++ src/edu/ynu/service/impl/UserServiceImpl.java
@@ -10,9 +10,14 @@ import edu.ynu.service.UserService;
 
 import java.io.File;
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import edu.ynu.util.TransformUtil;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,20 +28,35 @@ public class UserServiceImpl implements UserService {
     private UserDao userDao;
     @Autowired
     private ProjectDao projectDao;
+    public static void main(String[] args){
+
+    }
+    private String getCurrentProjectId(){
+        DetachedCriteria dc = DetachedCriteria.forClass(ProjectEntity.class);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dc.add(Restrictions.like("submitTime",dateFormat.format(new Date())+"%"));
+        Integer count = projectDao.countByCriteria(dc);
+        if(count==0){
+            return dateFormat.format(new Date()) + "01";
+        }else {
+            DetachedCriteria dc1 = DetachedCriteria.forClass(ProjectEntity.class);
+            dc1.setProjection(Projections.max("projectId"));
+            dc1.add(Restrictions.like("submitTime",dateFormat.format(new Date())+"%"));
+            String lastNumStr = String.valueOf(projectDao.listByCriteria(dc1).get(0));
+            Integer currentNum = Integer.valueOf(lastNumStr) + 1;
+            return String.valueOf(currentNum);
+        }
+    }
     private Integer savePurchaseApply(String userId, PurchaseApplySubmit pas,String status){
         ProjectEntity projectEntity = TransformUtil.toEntity(pas);
         projectEntity.setUserId(userId);
         String now = String.valueOf(new Timestamp(new Date().getTime()));  //当前时间
         projectEntity.setSubmitTime(now);
         projectEntity.setStatus(status);
-        Random random = new Random();
-        String projectNum = "k" + random.nextInt(9000);
-        projectEntity.setProjectId(projectNum);
-        try{
-            projectDao.savePurchaseApply(projectEntity);
-        }catch (Exception e){
-            return 0;
+        if(!status.equals("草稿")){
+            projectEntity.setProjectId(getCurrentProjectId());
         }
+        projectDao.save(projectEntity);
         return 1;
     }
     @Override
