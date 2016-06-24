890b85934484e08939625ca45f978a86fa90e0ab
Cookie4Cat <guozy42@foxmail.com>
2016-06-24 15:25:29 +0800

    修改实现

--- src/edu/ynu/service/impl/TeacherServiceImpl.java
+++ src/edu/ynu/service/impl/TeacherServiceImpl.java
@@ -1,15 +1,17 @@
 package edu.ynu.service.impl;
 
 import edu.ynu.dao.ProjectDao;
-import edu.ynu.dao.impl.BaseDao;
-import edu.ynu.dao.impl.ProjectDaoImpl;
 import edu.ynu.entity.ProjectEntity;
 import edu.ynu.message.PurchaseApplySubmit;
 import edu.ynu.service.TeacherService;
 import edu.ynu.util.TransformUtil;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 @Transactional
@@ -17,25 +19,63 @@ public class TeacherServiceImpl implements TeacherService{
     @Autowired
     private ProjectDao projectDao;
     @Override
-    public Integer findSubmitHistoryByIdCompletedPageCount(String userId) {
-        return null;
-    }
-
-    @Override
     public PurchaseApplySubmit findDraftByUID(String userId) {
-        List<ProjectEntity> draft = projectDao.findProjectsDraftByUID(userId);
-        return TransformUtil.toMessage(draft.get(0));
+        DetachedCriteria dc = DetachedCriteria.forClass(ProjectEntity.class);
+        dc.add(Restrictions.eq("userId",userId));
+        return TransformUtil.toMessage(projectDao.findByCriteria(dc));
     }
-
     @Override
-    public Integer saveDraftByUID(String userId, PurchaseApplySubmit submit) {
+    public void saveDraftByUID(String userId, PurchaseApplySubmit submit) {
         ProjectEntity entity = TransformUtil.toEntity(submit);
-        projectDao.saveProjectDraftByUID(userId,entity);
-        return 1;
+        entity.setStatus("草稿");
+        projectDao.save(entity);
     }
 
     @Override
-    public Integer findSubmitHistoryByIdUnCompletedPageCount(String userId) {
-        return null;
+    public PurchaseApplySubmit findByPID(String pid) {
+        DetachedCriteria dc = DetachedCriteria.forClass(ProjectEntity.class);
+        dc.add(Restrictions.eq("projectId",pid));
+        return TransformUtil.toMessage(projectDao.findByCriteria(dc));
+    }
+
+    @Override
+    public List<PurchaseApplySubmit> listHandlingProjects(String teacherId, Integer countPerPage, Integer pageNum) {
+        DetachedCriteria dc = DetachedCriteria.forClass(ProjectEntity.class);
+        String[] status = {"待审核","初审被驳","待立项"};
+        dc.add(Restrictions.in("status",status));
+        return TransformUtil.transformToMessageList(projectDao.listByCriteria(dc,countPerPage,pageNum));
+    }
+
+    @Override
+    public Integer countHandingProjects(String teacherId) {
+        DetachedCriteria dc = DetachedCriteria.forClass(ProjectEntity.class);
+        String[] status = {"待审核","初审被驳","待立项"};
+        dc.add(Restrictions.in("status",status));
+        return projectDao.countByCriteria(dc);
+    }
+
+    @Override
+    public List<PurchaseApplySubmit> listHistorySubmit(String teacherId, Integer count, Integer pageNum) {
+        DetachedCriteria dc = DetachedCriteria.forClass(ProjectEntity.class);
+        String[] status = {"采购完成"};
+        dc.add(Restrictions.in("status",status));
+        return TransformUtil.transformToMessageList(projectDao.listByCriteria(dc,count,pageNum));
+    }
+
+    @Override
+    public Integer countHistorySubmit(String teacherId) {
+        DetachedCriteria dc = DetachedCriteria.forClass(ProjectEntity.class);
+        String[] status = {"采购完成"};
+        dc.add(Restrictions.in("status",status));
+        return projectDao.countByCriteria(dc);
+    }
+
+    @Override
+    public void submitPurchaseApply(PurchaseApplySubmit submit,String userId) {
+        ProjectEntity entity = TransformUtil.toEntity(submit);
+        entity.setUserId(userId);
+        String now = String.valueOf(new Timestamp(new Date().getTime()));  //当前时间
+        entity.setSubmitTime(now);
+        entity.setStatus("待审核");
     }
 }
