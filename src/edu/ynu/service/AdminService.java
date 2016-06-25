977295763ab99fb9c61ebb1c5685256fee4812a9
Cookie4Cat <guozy42@foxmail.com>
2016-06-25 22:24:15 +0800

    增加接口方法

--- src/edu/ynu/service/AdminService.java
+++ src/edu/ynu/service/AdminService.java
@@ -6,11 +6,19 @@ import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 public interface AdminService {
+
     Integer countHandlingProjects();
     List<PurchaseApplySubmit> listHandlingProjects(Integer countPerPage, Integer currentPage);
     Integer countHandlingProjectsByCondition(String pid,String type,String status);
     List<PurchaseApplySubmit> listHandlingProjectsByCondition(String pid, String type, String status,
                                                               Integer countPerPage,Integer pageNum);
+
+    Integer countHistoryProjects();
+    List<PurchaseApplySubmit> listHistoryProjects(Integer countPerPage, Integer currentPage);
+    Integer countHistoryProjectsByCondition(String pid,String type,String status);
+    List<PurchaseApplySubmit> listHistoryProjectsByCondition(String pid, String type, String status,
+                                                             Integer countPerPage,Integer pageNum);
+
     PurchaseApplySubmit findOneProject(String projectId);
     void addProjectSuggestion(String projectId,String suggestion,String result);
     void setProjectUp(String projectId);
