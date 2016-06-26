32e981e0741fbe7fc255a4d899b19168c9263d85
Cookie4Cat <guozy42@foxmail.com>
2016-06-26 23:21:27 +0800

    rename

--- src/edu/ynu/message/PlanSubmit.java
+++ src/edu/ynu/message/PlanSubmit.java
@@ -6,7 +6,7 @@ public class PlanSubmit {
     //采购计划的编号
     private String planId;
     //拟批复形式
-    private String preReplyType;
+    private String preOrgType;
     //拟采购方式
     private String prePurchaseType;
     //批复形式
@@ -17,4 +17,60 @@ public class PlanSubmit {
     private String time;
     //批次勾选的项目id列表
     List<String> projectIdList;
+
+    public String getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(String planId) {
+        this.planId = planId;
+    }
+
+    public String getPreOrgType() {
+        return preOrgType;
+    }
+
+    public void setPreOrgType(String preOrgType) {
+        this.preOrgType = preOrgType;
+    }
+
+    public String getPrePurchaseType() {
+        return prePurchaseType;
+    }
+
+    public void setPrePurchaseType(String prePurchaseType) {
+        this.prePurchaseType = prePurchaseType;
+    }
+
+    public String getPurchaseType() {
+        return purchaseType;
+    }
+
+    public void setPurchaseType(String purchaseType) {
+        this.purchaseType = purchaseType;
+    }
+
+    public String getReplyType() {
+        return replyType;
+    }
+
+    public void setReplyType(String replyType) {
+        this.replyType = replyType;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public List<String> getProjectIdList() {
+        return projectIdList;
+    }
+
+    public void setProjectIdList(List<String> projectIdList) {
+        this.projectIdList = projectIdList;
+    }
 }
