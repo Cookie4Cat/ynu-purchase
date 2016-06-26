57a086e11150e7ecf77899ea6d7117f3d1e5dbb9
Cookie4Cat <guozy42@foxmail.com>
2016-06-26 12:03:17 +0800

    init

---
+++ src/edu/ynu/message/SampleMessage.java
@@ -0,0 +1,24 @@
+package edu.ynu.message;
+
+public class SampleMessage {
+    //内容
+    private String content;
+    //标记
+    private String flag;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+}
