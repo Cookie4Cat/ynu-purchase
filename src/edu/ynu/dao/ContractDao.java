8eda1da887706901bc0912f0320d9d6e64d48355
Cookie4Cat <guozy42@foxmail.com>
2016-06-27 19:22:19 +0800

    增加新街口

--- src/edu/ynu/dao/ContractDao.java
+++ src/edu/ynu/dao/ContractDao.java
@@ -1,7 +1,9 @@
 package edu.ynu.dao;
 
 import edu.ynu.entity.ContractEntity;
+import org.hibernate.criterion.DetachedCriteria;
 
 public interface ContractDao {
     void save(ContractEntity contractEntity);
+    ContractEntity findByCriteria(DetachedCriteria criteria);
 }
