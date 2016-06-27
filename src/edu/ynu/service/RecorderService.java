e9a61e96c1b5d9688585a2c678e278e6ada4e37a
Cookie4Cat <guozy42@foxmail.com>
2016-06-27 12:24:36 +0800

    fix bug

--- src/edu/ynu/service/RecorderService.java
+++ src/edu/ynu/service/RecorderService.java
@@ -2,6 +2,7 @@ package edu.ynu.service;
 
 import edu.ynu.entity.ContractEntity;
 import edu.ynu.entity.PlanEntity;
+import edu.ynu.message.PlanMessage;
 import edu.ynu.message.PlanSubmit;
 import edu.ynu.message.PurchaseApplySubmit;
 import edu.ynu.message.PurchaseItem;
@@ -11,12 +12,12 @@ import java.util.List;
 public interface RecorderService {
     void submitPlan(PlanSubmit submit);
     Integer countHandlingPlan();
-    List<PlanEntity> listHandlingPlan(Integer countPerPage,Integer pageNum);
+    List<PlanMessage> listHandlingPlan(Integer countPerPage, Integer pageNum);
     Integer countHistoryPlan();
-    List<PlanEntity> listHistoryPlan(Integer countPerPage,Integer pageNum);
+    List<PlanMessage> listHistoryPlan(Integer countPerPage,Integer pageNum);
     void replyPlan(PlanSubmit submit);
     void dropPlan(String planId);
-    PlanEntity findByPlanId(String planId);
+    PlanMessage findByPlanId(String planId);
     void submitContract(ContractEntity contract);
     List<PurchaseApplySubmit> listProjectsListSetUp();
 }
