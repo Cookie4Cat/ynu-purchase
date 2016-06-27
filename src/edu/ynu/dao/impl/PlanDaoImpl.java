70268481b6e0fde1a0e55f1b77e23a43f2ed9a7e
Cookie4Cat <guozy42@foxmail.com>
2016-06-27 17:55:19 +0800

    增加方法

--- src/edu/ynu/dao/impl/PlanDaoImpl.java
+++ src/edu/ynu/dao/impl/PlanDaoImpl.java
@@ -3,6 +3,8 @@ package edu.ynu.dao.impl;
 import edu.ynu.dao.PlanDao;
 import edu.ynu.entity.PlanEntity;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class PlanDaoImpl extends BaseDao<PlanEntity> implements PlanDao {
@@ -10,5 +12,12 @@ public class PlanDaoImpl extends BaseDao<PlanEntity> implements PlanDao {
     @Autowired
     public PlanDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
+    }
+
+    @Override
+    public PlanEntity findByPId(String planID) {
+        DetachedCriteria dc = DetachedCriteria.forClass(PlanEntity.class);
+        dc.add(Restrictions.eq("planId",planID));
+        return findByCriteria(dc);
     }
 }
