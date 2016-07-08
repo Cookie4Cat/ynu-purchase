605cc02ea54796b926512fde81626461daec346e
Cookie4Cat <guozy42@foxmail.com>
2016-07-08 18:57:45 +0800

    增加状态

--- src/edu/ynu/service/impl/AdminServiceImpl.java
+++ src/edu/ynu/service/impl/AdminServiceImpl.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class AdminServiceImpl implements AdminService {
     @Autowired
     private ProjectDao projectDao;
-    private final String[] historyStatusList = {"已立项","待采购","采购完成"};
+    private final String[] historyStatusList = {"被驳历史","已立项","待采购","采购完成"};
     private final String[] handlingStatusList = {"待立项","待审核"};
 
     private Integer countProjectsByStatus(String[] statusList){
