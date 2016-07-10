d2494a1d60d1f26c001dc1ff2d4b26e768651bec
Cookie4Cat <guozy42@foxmail.com>
2016-07-10 15:31:13 +0800

    init

---
+++ src/edu/ynu/message/LoginMessage.java
@@ -0,0 +1,22 @@
+package edu.ynu.message;
+
+public class LoginMessage {
+    int type;
+    String token;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
